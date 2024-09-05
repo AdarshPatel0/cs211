7511b01864c32502e13cfa1b1d58590fe59c2986
AdarshPatel0 <adarsh2005@outlook.com>
2024-09-05 00:04:15 -0400

    Initial Commit

---
+++ labs/Review/lab.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int size = 10;
+	double array[size] = {};
+	cout << "Enter ten numbers: ";
+	for (int i = 0; i < size; i++)
+	{
+		cin >> array[i];
+	}
+	double sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += array[i];
+	}
+	double average = sum / size;
+	cout << "Average of entries in array: " << average << endl;
+	double newSum = 0;
+	int divisor = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] > average)
+		{
+			newSum += array[i];
+			divisor++;
+		}
+	}
+	cout << "Average of entries greater than overall average: " << newSum / divisor << endl;
+	return 0;
+}
