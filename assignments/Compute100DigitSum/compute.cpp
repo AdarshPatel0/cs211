279da104a25449ec613d5e91e705ce9e5f07d67a
AdarshPatel0 <adarsh2005@outlook.com>
2024-09-05 17:13:47 -0400

    commit

--- assignments/Compute100DigitSum/compute.cpp
+++ assignments/Compute100DigitSum/compute.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
+#include <istream>
+#include <string>
 
 using namespace std;
 
 int maiin(){
-    int const digits = 100;
-    int numA[digits] = {};
-    int numB[digits] = {};
-    cout << "First integer (up to 100 digits):";
-    for (int i = digits-1; i!=-1;i--){
-        if(cin.eof()) break;
-        
-    }
-    return 0;
+	int const digits = 100;
+	char numA[digits] = {};
+	char numB[digits] = {};
+	cout << "First integer (up to 100 digits):";
+	getline(cin, numA);
+	return 0;
 }
