907d1d8016026018abe0583cefb52c92e3c46bbd
AdarshPatel0 <adarsh2005@outlook.com>
2024-09-05 01:47:27 -0400

    Commit

---
+++ assignments/Compute100DigitSum/compute.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int maiin(){
+    int const digits = 100;
+    int numA[digits] = {};
+    int numB[digits] = {};
+    cout << "First integer (up to 100 digits):";
+    for (int i = digits-1; i!=-1;i--){
+        if(cin.eof()) break;
+        
+    }
+    return 0;
+}
