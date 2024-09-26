2c0b56370bf2c8b0e4c0d19c603bdb14d1de9da4
AdarshPatel0 <adarsh2005@outlook.com>
2024-09-26 04:00:54 -0400

    commit

--- projects/8QueenProblem/1dBacktrackMethod.cpp
+++ projects/8QueenProblem/1dBacktrackMethod.cpp
@@ -1,39 +1,43 @@
 #include <iostream>
-
 using namespace std;
-
 int main(){
-	int counter = 0;
-	int q[8]={},c=0;
-	q[0]=0;
-nc: c++;
-	if(c==8) goto print;
-	cout << "Checking on column " << c << endl;
-	q[c] = -1;
-	goto nr;
-nr: q[c]++;
-	cout << "Checking column "<< c << " with value of " << q[c] << endl;
-	if(q[c]==8){
-		cout << "Exceeded rows left!" << endl;
-		goto backtrack;
-	}
-	for(int i = c-1;i>=0;i--){
-		if(q[i]==q[c]) goto nr;
-	}
-	goto nc;
-backtrack: c--;
-	if(c==-1) return 0;
-	q[c]=-1;
-	goto nc;
+    int solutionc = 0;
+    int c = -1;
+    int q[8]={};
+nc:
+    c++;
+    if(c==8){
+        goto print;
+    };
+    q[c]=-1;
+nr:
+    q[c] = q[c]+1;
+    if(q[c]==8){
+        goto backtrack;
+    }
+    for(int i = 0; i<c; i++){
+        if(q[i]==q[c]){
+            goto nr;
+        }
+        if((c-i == q[c]-q[i])||(c-i == -(q[c]-q[i]))){
+            goto nr;
+        }
+    }
+    goto nc;
+backtrack:
+    c--;
+    if(c==-1){
+        return 0;
+    }
+    goto nr;
 print:
-	counter++;
+    solutionc++;
 	for(int i = 0;i<8;i++){
 		for(int j = 0;j<8;j++){
 			cout << ((j==q[i])?"1 ":"0 ");
 		}
 		cout << endl;
 	}
-	cout << "Solution " << counter << endl;
-	cout << endl;
-	goto backtrack;
+    cout << solutionc << endl << endl;
+    goto backtrack;
 }
