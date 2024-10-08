3e48e9156263c1d57493b641c2c52dc9718f006e
AdarshPatel0 <adarsh2005@outlook.com>
2024-10-08 03:02:07 -0400

    commit

---
+++ projects/EightNumbers/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int counter = 0;
+    int q[8]={};
+    int c = -1;
+    int a[8][5] = {
+        {-1},
+        {0,-1},
+        {0,-1},
+        {0,1,2,-1},
+        {0,1,3,-1},
+        {1,4,-1},
+        {2,3,4,-1},
+        {3,4,5,6,-1}
+    };
+nc: c++;
+    if(c==8) goto print;
+nr: q[c]++;
+    if(q[c]==9) goto backtrack;
+    for(int i=0;i<c;i++){
+        if(q[c]==q[i]) goto nr;
+    }
+    for(int i = 0;i<5;i++){
+        if(a[c][i]==-1) break;
+        if(abs(q[c]-q[a[c][i]])==1) goto nr;
+    }
+    goto nc;
+backtrack:
+    q[c]=0;
+    c--;
+    if(c==-1){
+        cout << counter << endl;
+        return 0;
+    }
+    goto nr;
+print:
+    counter++;
+    for(int i = 0; i < 8; i++){
+        cout << q[i];
+    }
+    cout << endl;
+    goto backtrack;
+}
