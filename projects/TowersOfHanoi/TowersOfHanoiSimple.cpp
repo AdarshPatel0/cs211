e74caa1f1f6e0e4fadff5f4f1159a377aae52ad4
Adarsh Patel <adarsh2005@outlook.com>
2024-11-21 04:46:32 -0500

    commit

--- projects/TowersOfHanoi/TowersOfHanoiSimple.cpp
+++ projects/TowersOfHanoi/TowersOfHanoiSimple.cpp
@@ -2,32 +2,26 @@
 #include <vector>
 
 using namespace std;
-
-const int disks = 4; //These values will remain the same throught the program, so we use const
-const int NextTower = (disks%2==0)?2:1; //These ternary operations will allow us to set the direction of the algorithm
-const int OtherTower = (disks%2==0)?1:2;
-
-static int from = 0, to = (disks%2==0)?2:1, canidate = 1, moves = 0; //These Values will be chaning so we use static
-
-void printTowers(vector<int>t[3]){
-	for(int i = 0; i < 3;i++){//Loop throught the array of vectors
-		cout << (char)(i+'A'); //Print the towers name
-		for(int j = 0; j < t[i].size(); j++){ //Loop throught the vector of disks
-			if(t[i][j] != disks+1) cout << "|" << t[i][j]; //Onlt print the disk if it is not a padded value
-		}
-		cout << endl;
-	}
-}
-
-int main(){	
+int main(){
+	int disks;
+	cout << "Amount of disks: "; //Create variable that holds amount of disks and ask use to give it a value
+	cin >> disks;
+	int NextTower = (disks%2==0)?2:1, OtherTower = (disks%2==0)?1:2; //These ternary operations will allow us to set the direction of the algorithm
+	int from = 0, to = (disks%2==0)?2:1, canidate = 1, moves = 0;
 	vector <int> towers[3];
-	for(int i = 0; i < 3; i++){ //Pad the bottom of the towers will values that are one bigger then the biggest disk value
+	for(int i = 0; i < 3; i++){ //Pad the bottom of the towers with values that are one bigger then the biggest disk value
 		towers[i].push_back(disks+1);
 	}
 	for(int i = disks; i > 0; i--){ //Populate tower A with disks
 		towers[0].push_back(i);
 	}
-	printTowers(towers);
+	for(int i = 0; i < 3;i++){//Loop throught the array of vectors
+		cout << (char)(i+'A'); //Print the towers name
+		for(int j = 0; j < towers[i].size(); j++){ //Loop throught the vector of disks
+			if(towers[i][j] != disks+1) cout << "|" << towers[i][j]; //Only print the disk if it is not a padded value
+		}
+		cout << endl;
+	}
 	while(towers[1].size()<disks+1){
 		moves++; //Increase move counter
 		towers[to].push_back(canidate); //Put the canidate on the to tower
@@ -35,7 +29,14 @@ int main(){
 
 		cout << "Step " << moves << ": Move " << canidate << " from " << char(from+'A') << " to " << char(to+'A') << endl;
 
-		printTowers(towers);
+		for(int i = 0; i < 3;i++){//Loop throught the array of vectors
+			cout << (char)(i+'A'); //Print the towers name
+			for(int j = 0; j < towers[i].size(); j++){ //Loop throught the vector of disks
+				if(towers[i][j] != disks+1) cout << "|" << towers[i][j]; //Only print the disk if it is not a padded value
+			}
+			cout << endl;
+		}
+
 		if(towers[(to+NextTower)%3].back() > towers[(to+OtherTower)%3].back()){ //If the top of the next tower is bigger then the top of the other tower, then set the from tower to the other tower since it has a smaller disk on top
 			from = (to+OtherTower)%3;
 		}
