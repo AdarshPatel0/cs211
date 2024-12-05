0f82ca530d953b171593a837efbe19ce81b16fe3
Adarsh Patel <adarsh2005@outlook.com>
2024-12-05 07:50:53 -0500

    commit

---
+++ extra/wfc/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <unistd.h>
+
+using namespace std;
+
+class cup{
+	public:
+	int size;
+};
+
+class coffeeCup : public cup{
+	public:
+	string liquid = "Coffee";
+	coffeeCup(){
+		size = 1;
+	}
+	coffeeCup(int s){
+		size = s;
+	}
+};
+
+int main(){
+	coffeeCup myCoffee(3);
+	cout << myCoffee.size << " oz of " << myCoffee.liquid << endl;
+	return 0;
+}
